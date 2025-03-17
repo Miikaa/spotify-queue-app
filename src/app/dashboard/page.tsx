7a88234eed0393bf1b6ce03bc5b6880ac6d39694
Miika Hagelberg <Miika.hagelberg@gmail.com>
2025-03-17 18:57:24 +0200

    Add refresh buttons to Dashboard for current track and queue; enhance user interaction

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -812,6 +812,18 @@ export default function Dashboard() {
                     {isLoading ? '...' : (roomId ? 'Destroy' : 'Create')}
                   </button>
                   <button
+                    onClick={() => {
+                      fetchCurrentTrack();
+                      fetchQueue();
+                    }}
+                    className="p-2 text-white hover:text-[#1DB954] transition-colors"
+                    title="Refresh"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                  <button
                     onClick={handleSignOut}
                     disabled={isLoading}
                     className={`px-3 py-2 bg-[#282828] text-white text-sm rounded-lg hover:bg-[#383838] transition-colors ${
@@ -848,6 +860,18 @@ export default function Dashboard() {
                     } ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
                   >
                     {isLoading ? 'Processing...' : (roomId ? 'Destroy Room' : 'Create Room')}
+                  </button>
+                  <button
+                    onClick={() => {
+                      fetchCurrentTrack();
+                      fetchQueue();
+                    }}
+                    className="p-2 text-white hover:text-[#1DB954] transition-colors"
+                    title="Refresh"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
+                      <path fillRule="evenodd" d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" clipRule="evenodd" />
+                    </svg>
                   </button>
                   <button
                     onClick={handleSignOut}
